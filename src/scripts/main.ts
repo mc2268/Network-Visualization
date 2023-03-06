2f32ddcef99a91158233140a2be589dd209668ee
马成 <81601669+mc2268@users.noreply.github.com>
2023-03-06 15:38:11 +0800

    Update main.ts

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -2,8 +2,8 @@
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
 
-import App from './App.vue';
-import router from './router';
+import App from '../App.vue';
+import router from '../router';
 
 import AntdV from 'ant-design-vue';
 import 'ant-design-vue/dist/antd.css';
