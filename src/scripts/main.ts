ecf8a5d58aabf4b90a096caa74a685b5ccac63ae
马成 <81601669+mc2268@users.noreply.github.com>
2023-03-06 15:33:09 +0800

    Update and rename main.js to main.ts

---
+++ src/scripts/main.ts
@@ -0,0 +1,16 @@
+
+import { createApp } from 'vue';
+import { createPinia } from 'pinia';
+
+import App from './App.vue';
+import router from './router';
+
+import AntdV from 'ant-design-vue';
+import 'ant-design-vue/dist/antd.css';
+
+const app = createApp(App);
+app.use(createPinia());
+app.use(router);
+app.use(AntdV);
+
+app.mount('#app');
