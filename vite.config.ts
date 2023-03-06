333f4a64a194c4b5c75f4f55fd3800a6dd29c9fc
马成 <81601669+mc2268@users.noreply.github.com>
2023-03-06 16:01:49 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -7,9 +7,6 @@ import AutoImport from 'unplugin-auto-import/vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  module.exports: {
-  publicPath: "./"
-},
   server: {
     host: 'localhost',
     port: 3090,
